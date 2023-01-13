6eecfd78124a4883e389c140d83d4b7761510f1e
Salvador Murillo <salmur2k@gmail.com>
2023-01-13 15:20:33 -0500

    Button layout and functionality completed

--- calc.py
+++ calc.py
@@ -6,34 +6,28 @@
 import tkinter as tk
 from decimal import Decimal
 
-# Key lock object used to force number input after operation input
-# False state -> Unlocked; True state -> Locked
-class keyLock:
-    def __init__(self):
-        self.state = False
-
 class calcApp:
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("640x480")
         self.root.title("MyCalculator")
 
-        # Create area where history is displayed
+        self.displayFrame = tk.Frame(master= self.root)
+
+        # Create label where history is displayed
         self.historyText = tk.StringVar(master= self.root, value= "")
-        self.historyDisplay = tk.Label(master= self.root, textvariable= self.historyText)
-        self.historyDisplay.pack()
+        self.historyDisplay = tk.Label(master= self.displayFrame, textvariable= self.historyText, anchor= tk.E)
+        self.historyDisplay.pack(expand= True, fill= "both", pady= 10)
 
-        # Create area where results and input are displayed
+        # Create label where results and input are displayed
         self.entryText = tk.StringVar(master= self.root, value= "0")
-        self.entryDisplay = tk.Label(master= self.root, textvariable= self.entryText)
-        self.entryDisplay.pack()
+        self.entryDisplay = tk.Label(master= self.displayFrame, textvariable= self.entryText, anchor= tk.E)
+        self.entryDisplay.pack(expand= True, fill= "both", pady= 10)
 
-        # For calculating entry with the recently done operation
-        self.recentOp = ""
-        self.recentNum = "0"
+        self.displayFrame.pack(anchor= tk.E)
 
         # Create lock for operation keys, default value is False
-        self.keyLock = keyLock()
+        self.keyLock = False
 
         # Create Frame that contains button keys
         self.keyPad = tk.Frame(master= self.root)
@@ -50,56 +44,50 @@ class calcApp:
         for i in range(3):
             offset = 2
             for j in range(3):
-                self.numKeys[str(key - offset)].grid(row= (i + 2), column= j, sticky=(tk.W + tk.E))
+                self.numKeys[str(key - offset)].grid(row= (i + 2), column= j, sticky=tk.NSEW)
                 offset -= 1
 
             key -= 3
 
-        self.numKeys["0"].grid(row= 5, column= 1, sticky= (tk.W + tk.E))
+        self.numKeys["0"].grid(row= 5, column= 1, sticky= tk.NSEW)
 
         # Create dictionary for keys that are not for numbers
         self.keys = {}
 
-        self.keys["BACKSPACE"] = tk.Button(master= self.keyPad, text= "DEL", command= self.deleteSingle)
-        self.keys["BACKSPACE"].grid(row= 0, column= 2, sticky= (tk.W + tk.E))
+        self.keys["BACKSPACE"] = tk.Button(master= self.keyPad, text= u"\u2190", command= self.deleteSingle)
+        self.keys["BACKSPACE"].grid(row= 0, rowspan= 2, column= 0, sticky= tk.NSEW)
 
-        self.keys["CLEAR"] = tk.Button(master= self.keyPad, text= "CLEAR", command= self.deleteAll)
-        self.keys["CLEAR"].grid(row= 1, column= 2, sticky= (tk.W + tk.E))
+        self.keys["CE"] = tk.Button(master= self.keyPad, text= "CLR ENTR", command= self.deleteEntry)
+        self.keys["CE"].grid(row= 0, rowspan= 2, column= 1, sticky= tk.NSEW)
 
-        self.keys["+/-"] = tk.Button(master= self.keyPad, text= "+/-", command= self.test)
-        self.keys["+/-"].grid(row= 5, column= 0, sticky= (tk.W + tk.E))
+        self.keys["CH"] = tk.Button(master= self.keyPad, text= "CLR HIST", command= self.deleteHistory)
+        self.keys["CH"].grid(row= 0, rowspan= 2, column= 2, sticky= tk.NSEW)
 
-        self.keys["("] = tk.Button(master= self.keyPad, text= "(", command= self.test)
-        self.keys["("].grid(row= 1, column= 0, sticky= (tk.W + tk.E))
+        self.keys["+/-"] = tk.Button(master= self.keyPad, text= "+/-", command= self.switchSign)
+        self.keys["+/-"].grid(row= 5, column= 0, sticky= tk.NSEW)
 
-        self.keys[")"] = tk.Button(master= self.keyPad, text= ")", command= self.test)
-        self.keys[")"].grid(row= 1, column= 1, sticky= (tk.W + tk.E))
-
-        self.keys["."] = tk.Button(master= self.keyPad, text= ".", command= self.test)
-        self.keys["."].grid(row= 5, column= 2, sticky= (tk.W + tk.E))
+        self.keys["."] = tk.Button(master= self.keyPad, text= ".", command= self.insertDecimal)
+        self.keys["."].grid(row= 5, column= 2, sticky= tk.NSEW)
 
         self.keys["/"] = tk.Button(master= self.keyPad, text= "/", command= lambda: self.insertOperator("/"))
-        self.keys["/"].grid(row= 0, column= 3, sticky= (tk.W + tk.E))
+        self.keys["/"].grid(row= 0, column= 3, sticky= tk.NSEW)
 
         self.keys["*"] = tk.Button(master= self.keyPad, text= "*", command= lambda: self.insertOperator("*"))
-        self.keys["*"].grid(row= 1, column= 3, sticky= (tk.W + tk.E))
+        self.keys["*"].grid(row= 1, column= 3, sticky= tk.NSEW)
 
         self.keys["-"] = tk.Button(master= self.keyPad, text= "-", command= lambda: self.insertOperator("-"))
-        self.keys["-"].grid(row= 2, column= 3, sticky= (tk.W + tk.E))
+        self.keys["-"].grid(row= 2, column= 3, sticky= tk.NSEW)
 
         self.keys["+"] = tk.Button(master= self.keyPad, text= "+", command= lambda: self.insertOperator("+"))
-        self.keys["+"].grid(row= 3, column= 3, sticky= (tk.W + tk.E))
+        self.keys["+"].grid(row= 3, column= 3, sticky= tk.NSEW)
 
         self.keys["="] = tk.Button(master= self.keyPad, text= "=", command= self.equalOp)
-        self.keys["="].grid(row= 4, rowspan= 2, column= 3, sticky= (tk.NSEW))
+        self.keys["="].grid(row= 4, rowspan= 2, column= 3, sticky= tk.NSEW)
 
         # Pack keyPad frame
         self.keyPad.pack(fill="x")
 
         self.root.mainloop()
-
-    def test(self):
-        pass
 
     # Function that changes button background color switching
     def changeButtonColor(self, state):
@@ -107,7 +95,7 @@ class calcApp:
 
         if state is True:
             for i in items:
-                self.keys[i].configure(bg= "red")
+                self.keys[i].configure(bg= "yellow")
 
         if state is False:
             for i in items:
@@ -117,22 +105,21 @@ class calcApp:
     def insertEntry(self, entry):
         history = self.historyText.get()
 
-        if len(history) > 0 and history[len(history) - 1] == "=":
+        if history.find("=") != -1:
             self.historyText.set("")
             self.entryText.set(entry)
-            self.recentNum = entry
-            self.keyLock.state = False
-            self.changeButtonColor(self.keyLock.state)
 
-        elif self.keyLock.state is True:
+            self.keyLock = False
+            self.changeButtonColor(self.keyLock)
+
+        elif self.keyLock is True:
             self.entryText.set(entry)
-            self.recentNum = entry
-            self.keyLock.state = False
-            self.changeButtonColor(self.keyLock.state)
+
+            self.keyLock = False
+            self.changeButtonColor(self.keyLock)
 
         else:
             newEntry = self.entryText.get().replace(",", "") +entry
-            self.recentNum = "{:,}".format(Decimal(newEntry))
             self.entryText.set("{:,}".format(Decimal(newEntry)))
 
     # Function that handles the insertion of operators being inserted
@@ -140,68 +127,107 @@ class calcApp:
         history = self.historyText.get()
         entry = self.entryText.get()
 
-        if len(history) > 0 and history[len(history) - 1] == "=":
+        if entry == "ERROR":
+            pass
+
+        elif history.find("=") != -1:
             self.historyText.set(entry +" " +op)
-            self.recentOp = op
-            self.keyLock.state = True
-            self.changeButtonColor(self.keyLock.state)
 
+            self.keyLock = True
+            self.changeButtonColor(self.keyLock)
 
-        elif self.keyLock.state is True:
+        elif self.keyLock is True:
             self.historyText.set(history[0 : len(history) - 1] +op)
-            self.recentOp = op
 
         else:
-            self.keyLock.state = True
-            self.changeButtonColor(self.keyLock.state)
-            result = eval(history.replace(",", "") +entry.replace(",", ""))
-            self.recentOp = op
-            self.recentNum = "{:,}".format(result)
-            self.historyText.set(history +" " +entry +" " +op)
-            self.entryText.set("{:,}".format(result))
+            self.keyLock = True
+            self.changeButtonColor(self.keyLock)
+
+            try:
+                result = eval(history.replace(",", "") +entry.replace(",", ""))
+                self.historyText.set(history +" " +entry +" " +op)
+                self.entryText.set("{:,}".format(result))
+            except:
+                self.historyText.set("")
+                self.entryText.set("ERROR")
     
     # Function that handles the several behaviours of the '=' operation
     def equalOp(self):
         history = self.historyText.get()
         entry = self.entryText.get()
 
-        if self.recentOp == "":
-            pass
-        
-        elif len(history) > 0 and history[len(history) - 1] == "=":
-            result = eval(entry.replace(",", "") +self.recentOp +self.recentNum.replace(",", ""))
-            self.historyText.set(entry +" " +self.recentOp +" "  +self.recentNum +" =")
-            self.entryText.set("{:,}".format(result))
+        if history.find("=") != -1:
+            self.historyText.set("")
+            
+            self.keyLock = False
+            self.changeButtonColor(self.keyLock)
 
         else:
-            result = eval(history.replace(",", "") +entry.replace(",", ""))
-            self.historyText.set(history +" " +entry +" =")
-            self.entryText.set("{:,}".format(result))
+            try:
+                result = eval(history.replace(",", "") +entry.replace(",", ""))
+                self.historyText.set(history +" " +entry +" =")
+                self.entryText.set("{:,}".format(result))
+            except:
+                self.keyLock = True
+                self.changeButtonColor(self.keyLock)
+
+                self.historyText.set("")
+                self.entryText.set("ERROR")
+
+    # Toggle entry between positive or negative
+    def switchSign(self):
+        entry = self.entryText.get()
+
+        if entry == "0":
+            pass
+
+        elif entry.find("-") == 0:
+            self.entryText.set(entry[1:])
+
+        else:
+            self.entryText.set("-" +entry)
+
+    # Toggle between having a decimal or not for entry
+    def insertDecimal(self):
+        entry = self.entryText.get()
+
+        if entry.find(".") == -1:
+            entry = entry +"."
+            self.entryText.set(entry)
+        else:
+            entry = entry[0 : entry.find(".")]
+            self.entryText.set(entry)
 
     # Function that deletes 1-character from the entry text
     def deleteSingle(self):
         entry = self.entryText.get().replace(",", "")
         
         if len(entry) == 1 and entry == "0":
-            return None
+            pass
 
-        if len(entry) == 1 and entry != "0":
+        elif len(entry) == 1 and entry != "0":
             self.entryText.set("0")
-            return None
+        
+        elif len(entry) == 2 and entry.find("-") == 0:
+            self.entryText.set("0")
 
-        entry = "{:,}".format(Decimal(entry[:len(entry) - 1])) # Bug with negatives
-        self.recentNum = entry
-        self.entryText.set(entry)
+        else:
+            entry = "{:,}".format(Decimal(entry[0 : len(entry) - 1]))
+            self.entryText.set(entry)
 
-    # Function that handles clearing of entry and history
-    def deleteAll(self): # FIX! maybe add other button
-        entry = self.entryText.get()
-        if entry == "0":
-            self.historyText.set("")
-
+    # Function that clears entry
+    def deleteEntry(self):
         self.entryText.set("0")
-        self.keyLock.state = False
-        self.changeButtonColor(self.keyLock.state)
+
+        self.keyLock = False
+        self.changeButtonColor(self.keyLock)
+
+    # Function that clears history
+    def deleteHistory(self):
+        self.historyText.set("")
+
+        self.keyLock = False
+        self.changeButtonColor(self.keyLock)
     
 def main():
     app = calcApp()
